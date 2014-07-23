c97394244b29c8f6b950bd1494533da36414a068
Andy <Andy@10.146.123.135>
2014-07-22 21:09:42 -0500

    Minor changes and commenting.

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -2,61 +2,82 @@
  * 
  */
 function LoadPage() {
+	/* Removes old bound click events. */
+	$('#hiw-btn').unbind("click");
+	$('#au-btn').unbind("click");
+	$('#logo').unbind("click");
+	$('#cu-btn').unbind("click");
+	$('#si-btn').unbind("click");
+	$('#si-btn2').unbind("click");
+	$('#btn-signup').unbind("click");
+	$('#su-link').unbind("click");
+	$('#request-btn').unbind("click");
+	$('#save-btn').unbind("click");
 	
+	/* Sets click event behavior for the How It Works button. */
 	$('#hiw-btn').on("click", function() {
 		$('#page-content').load("pages/how_it_works.jsp", function() {
 			LoadPage();
 		});
 	});
 	
+	/* Sets click event behavior for the About Us button. */
 	$('#au-btn').on("click", function() {
 		$('#page-content').load("pages/about_us.jsp", function() {
 			LoadPage();
 		});
 	});
 	
+	/* Sets click event behavior for the Logo/Home button. */
 	$('#logo').on("click", function() {
 		$('#page-content').load("pages/landing.jsp", function() {
 			LoadPage();
 		});
 	});
 	
+	/* Sets click event behavior for the Contact Us button. */
 	$('#cu-btn').on("click", function() {
 		$('#page-content').load("pages/contact_us.jsp", function() {
 			LoadPage();
 		});
 	});
 	
+	/* Sets click event behavior for the Already a Member link. */
 	$('#si-btn').on("click", function() {
 		$('#page-content').load("pages/user_login.jsp", function() {
 			LoadPage();
 		});
 	});
 	
+	/* Sets click event behavior for the Sign Up button on the user sign up page. */
 	$('#si-btn2').on("click", function() {
 		$('#page-content').load("pages/user_profile.jsp", function() {
 			LoadPage();
 		});
 	});
 	
+	/* Sets click event behavior for the Sign Up button on the landing page. */
 	$('#btn-signup').on("click", function() {
 		$('#page-content').load("pages/sign-up.jsp", function() {
 			LoadPage();
 		});
 	});
 	
+	/* Sets click event behavior for the Sign Up link on the user sign in page. */
 	$('#su-link').on("click", function() {
 		$('#page-content').load("pages/sign-up.jsp", function() {
 			LoadPage();
 		});
 	});
 	
+	/* Sets click event behavior for transaction request button on the user profile page. */
 	$('#request-btn').on("click", function() {
 		$('#request-match').css("display", "block");
 		$('#next-request-match').css("display", "none");
 		$('#request-accept').css("display", "none");
 	});
 	
+	/* Sets click event behavior for the save settings button on the user profile page. */
 	$('#save-btn').on("click", function() {
 		var save = confirm("Are you sure you would like to save these settings as your default preferences? They will be automatically loaded in the provided boxes every tim you log into your Ladar account.");
 		if(save) {
@@ -66,12 +87,18 @@ function LoadPage() {
 	});
 }
 
+/**
+ * 
+ */
 function acceptLink() {
 	$('#request-match').css("display", "none");
 	$('#next-request-match').css("display", "none");
 	$('#request-accept').css("display", "block");
 }
 
+/**
+ * 
+ */
 function declineLink() {
 	$('#request-match').css("display", "none");
 	$('#next-request-match').css("display", "block");
