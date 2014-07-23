da2a7fefb210b8a101529aef49d72fb9f77dca64
Andy <Andy@Andy_Laptop.attlocal.net>
2014-07-22 19:26:51 -0500

    Removed unneeded features for beta list version.

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -27,52 +27,9 @@ function LoadPage() {
 		});
 	});
 	
-	$('#si-btn').on("click", function() {
-		$('#page-content').load("pages/user_login.jsp", function() {
-			LoadPage();
-		});
-	});
-	
-	$('#si-btn2').on("click", function() {
-		$('#page-content').load("pages/user_profile.jsp", function() {
-			LoadPage();
-		});
-	});
-	
 	$('#btn-signup').on("click", function() {
-		$('#page-content').load("pages/sign-up.jsp", function() {
-			LoadPage();
-		});
+//		$('#page-content').load("pages/sign-up.jsp", function() {
+//			LoadPage();
+//		});
 	});
-	
-	$('#su-link').on("click", function() {
-		$('#page-content').load("pages/sign-up.jsp", function() {
-			LoadPage();
-		});
-	});
-	
-	$('#request-btn').on("click", function() {
-		$('#request-match').css("display", "block");
-		$('#next-request-match').css("display", "none");
-		$('#request-accept').css("display", "none");
-	});
-	
-	$('#save-btn').on("click", function() {
-		var save = confirm("Are you sure you would like to save these settings as your default preferences? They will be automatically loaded in the provided boxes every tim you log into your Ladar account.");
-		if(save) {
-			/* Write preferences to user database. */
-		}
-		LoadPage();
-	});
-}
-
-function acceptLink() {
-	$('#request-match').css("display", "none");
-	$('#next-request-match').css("display", "none");
-	$('#request-accept').css("display", "block");
-}
-
-function declineLink() {
-	$('#request-match').css("display", "none");
-	$('#next-request-match').css("display", "block");
 }
