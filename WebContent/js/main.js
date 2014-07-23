1f15578536f2c7218239cc3610afec79621f3931
Andy <Andy@10.146.123.135>
2014-07-22 21:02:34 -0500

    Minor changes and commenting.

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -2,25 +2,34 @@
  * 
  */
 function LoadPage() {
+	/* Removes old bound click events. */
+	$('#hiw-btn').unbind("click");
+	$('#au-btn').unbind("click");
+	$('#logo').unbind("click");
+	$('#cu-btn').unbind("click");
 	
+	/* Sets click event behavior for the How It Works button. */
 	$('#hiw-btn').on("click", function() {
 		$('#page-content').load("pages/how_it_works.jsp", function() {
 			LoadPage();
 		});
 	});
 	
+	/* Sets click event behavior for the About Us button. */
 	$('#au-btn').on("click", function() {
 		$('#page-content').load("pages/about_us.jsp", function() {
 			LoadPage();
 		});
 	});
 	
+	/* Sets click event behavior for the Logo/Home button. */
 	$('#logo').on("click", function() {
 		$('#page-content').load("pages/landing.jsp", function() {
 			LoadPage();
 		});
 	});
 	
+	/* Sets click event behavior for the Contact Us button. */
 	$('#cu-btn').on("click", function() {
 		$('#page-content').load("pages/contact_us.jsp", function() {
 			LoadPage();
@@ -28,15 +37,23 @@ function LoadPage() {
 	});
 }
 
+/**
+ * 
+ */
 function AddToBeta() {
+	/* Serializes beta sign up form data. */
 	var formData = $('#signup-form').serialize();
 	
+	/* Makes ajax call to store new email in beta list. */
 	$.ajax({
 		url: "form_acceptor.jsp",
 		type: "POST",
 		data: formData,
 		async: false
 	});
+	
+	/* Loads the sign up success page into the page content div. */
 	$('#page-content').load("pages/signup_success.jsp");
+	
 	return false;
 }
